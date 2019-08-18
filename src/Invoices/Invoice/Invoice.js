a2d950971b5906370924eb8ea715eddd5dd6b357
nitin pawar <nitincp@users.noreply.github.com>
2019-08-18 18:51:08 +0530

    debug config added.

--- src/Invoices/Invoice/Invoice.js
+++ src/Invoices/Invoice/Invoice.js
@@ -5,7 +5,7 @@ import './Invoice.scss';
 export function Invoice(props) {
   return (
     <div>
-      <h1>Invoice</h1>
+      <h1>Invoice For Me.</h1>
       <p>I am at Invoice {props.invoiceId}</p>
     </div>
   );
