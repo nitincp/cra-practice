9082bfb6d7469213dc77ce80a7037f500ac0cbf0
nitin pawar <nitincp@users.noreply.github.com>
2019-08-18 19:50:51 +0530

    Updates

---
+++ precache-manifest.5fcf394706ca8d75fae79ab1796922da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6dc9592642a8affc17a73be8ad5f22",
+    "url": "/cra-practice/index.html"
+  },
+  {
+    "revision": "a4974adf4a897c4d7c9a",
+    "url": "/cra-practice/static/css/2.c2b180e8.chunk.css"
+  },
+  {
+    "revision": "1d56fd2f11b4eb564abe",
+    "url": "/cra-practice/static/css/main.c162c1c1.chunk.css"
+  },
+  {
+    "revision": "a4974adf4a897c4d7c9a",
+    "url": "/cra-practice/static/js/2.77cefdf5.chunk.js"
+  },
+  {
+    "revision": "1d56fd2f11b4eb564abe",
+    "url": "/cra-practice/static/js/main.7d10dc04.chunk.js"
+  },
+  {
+    "revision": "89308f7ef1c1c8cefa62",
+    "url": "/cra-practice/static/js/runtime~main.eab87c21.js"
+  }
+]);
--- src/Invoices/Invoices.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Invoices } from './Invoices';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Invoices />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
